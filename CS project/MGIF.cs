89bbf3c5e97e13832b4a90de63be7a5863e048be
Karaso Eran <karasoe@my.hit.ac.il>
2023-04-02 15:00:12 +0300

    Add project files.

---
+++ CS project/MGIF.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_project
+{
+    class MGIF//:Meme
+    {
+        private int frameCount;
+
+        public int FrameCount { get => frameCount; set => frameCount = value; }
+
+        public void play() { }
+        public void pause() { }
+    }
+}
